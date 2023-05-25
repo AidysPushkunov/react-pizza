4935db2d3bc26ac86b8af9c35a87e21e36580c25
Aidys Pushkunov <mywayinprogramming@gmail.com>
2023-05-25 21:35:04 +0700

    added typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,4 @@
-import React from 'react';
-
 import './scss/app.scss';
-import Header from './components/Header/Header';
 import Home from './pages/Home';
 import NotFound from './pages/NotFound';
 import { Routes, Route } from 'react-router-dom';
--- src/pages/FullPizza.tsx
+++ src/pages/FullPizza.tsx
@@ -2,33 +2,37 @@ import React from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import axios from "axios";
 
-const FullPizza = () => {
-    const [pizza, setPizza] = React.useState();
+const FullPizza: React.FC = () => {
+    const [pizza, setPizza] = React.useState<{
+        imageUrl: string;
+        title: string;
+        price: number;
+    }>();
     const { id } = useParams();
     const navigate = useNavigate();
 
     React.useEffect(() => {
         async function fetchPizza() {
             try {
-                const { data } = await axios.get('https://64457ab8914c816083cfb742.mockapi.io/items/' +  id);
+                const { data } = await axios.get('https://64457ab8914c816083cfb742.mockapi.io/items/' + id);
                 setPizza(data)
             } catch (error) {
                 alert('Ошибка при получении пиццы!')
                 navigate('/');
             }
-        } 
+        }
 
         fetchPizza();
     }, []);
 
-    if (!pizza) return 'Загрузка...';
+    if (!pizza) return <>Загрузка...</>;
 
     return (
-        
+
         <div className="container">
             <img src={pizza.imageUrl} alt="pizza" />
-            <h2>{ pizza.title }</h2>
-            <h4>{ pizza.price } ₽</h4>
+            <h2>{pizza.title}</h2>
+            <h4>{pizza.price} ₽</h4>
         </div>
     );
 }
