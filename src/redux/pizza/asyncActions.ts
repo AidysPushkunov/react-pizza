4784c24362b171501f00925541734d7757b182bd
Aidys Pushkunov <mywayinprogramming@gmail.com>
2023-06-05 10:37:43 +0700

    fixed bugs on sortType in pizza/slice.ts

--- src/redux/pizza/asyncActions.ts
+++ src/redux/pizza/asyncActions.ts
@@ -3,9 +3,9 @@ import { Pizza } from "./types";
 import axios from "axios";
 
 export const fetchPizzas = createAsyncThunk<Pizza[], Record<string, string>>('pizza/fetchPizzasStatus', async (params) => {
-    const { sortBy, orderType, categoryType, search, currentPage } = params;
+    const { sortType, orderType, categoryType, search, currentPage } = params;
     const { data } = await axios.get<Pizza[]>(
-        `https://64457ab8914c816083cfb742.mockapi.io/items?page=${currentPage}&limit=4&${categoryType}&sortBy=${sortBy}&order=${orderType}${search}`,
+        `https://64457ab8914c816083cfb742.mockapi.io/items?page=${currentPage}&limit=4&${categoryType}&sortBy=${sortType}&order=${orderType}${search}`,
     );
 
     return data;
